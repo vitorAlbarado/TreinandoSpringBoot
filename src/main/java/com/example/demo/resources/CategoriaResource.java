8f3153f07b25cd347769c0a438301a5fea73bf5c
Vitor Albarado <vitor.jardina@gmail.com>
2022-10-08 20:02:36 -0400

    criando endopoint categoria

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -1,25 +1,26 @@
 package com.example.demo.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat = new Categoria(1, "informática");
-		Categoria cat2 = new Categoria(2, "escritorio");
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat);
-		lista.add(cat2);
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
+		
+		return ResponseEntity.ok(obj);
 	}
 }
